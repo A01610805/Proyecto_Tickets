deca054a92ab4c934402c549d89dfe72cdf79876
Renato549 <A01275401@itesm.mx>
2022-03-11 00:27:24 -0600

    cambios menores a categoria

--- Desarrollo_Prototipo_App/barra.js
+++ Desarrollo_Prototipo_App/barra.js
@@ -3,8 +3,6 @@ let listElements = document.querySelectorAll('.list__button--click');
 listElements.forEach(listElement => {
     listElement.addEventListener('click', ()=>{
         
-        listElement.classList.toggle('arrow');
-
         let height = 0;
         let menu = listElement.nextElementSibling;
         if(menu.clientHeight == "0"){
