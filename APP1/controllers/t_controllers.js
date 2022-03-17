4f18d41fe06053f1adb977a0e79bd47d8bf20034
Renato549 <A01275401@itesm.mx>
2022-03-16 18:34:07 -0600

    modificaciones

--- APP1/controllers/t_controllers.js
+++ APP1/controllers/t_controllers.js
@@ -2,16 +2,57 @@ const Ticket = require("../models/tickets");
 
 exports.post_nuevo = (request, response, next) => {
 
-    const ticket = 
+    const tick = 
         new Ticket(
-            request.body.id, request.body.description, 
+            request.body.id, request.body.ti, request.body.cat, request.body.description, 
             request.body.pregunta_uno, request.body.pregunta_dos,  request.body.pregunta_tres);
-    ticket.save()
+    tick.save()
         .then(() => {
-            request.session.info = capybara.nombre + ' fue registrado con éxito';
+            request.session.info = tick.id + ' fue registrado con éxito';
             response.setHeader('Set-Cookie', 
-                'ultimo_capybara='+capybara.nombre+'; HttpOnly');
-            response.redirect('/capybaras');
+                'ultimo_capybara='+tick.id+'; HttpOnly');
+            response.redirect('/Primer pantalla');
         })
         .catch(err => console.log(err));
 };
+
+exports.listar = (request, response, next) => {
+    console.log('Ruta /capybaras');
+    //console.log(request.get('Cookie').split('=')[1]);
+    console.log(request.cookies);
+    const info = request.session.info ? request.session.info : '';
+    request.session.info = '';
+    Ticket.fetchAll()
+        .then(([rows, fieldData]) => {
+            console.log(rows);
+            response.render('lista', {
+                capybaras: rows,
+                username: request.session.username ? request.session.username : '',
+                ultimo_capybara: request.cookies.ultimo_capybara ? request.cookies.ultimo_capybara : '',
+                info: info //El primer info es la variable del template, el segundo la constante creada arriba
+            }); 
+        })
+        .catch(err => {
+            console.log(err);
+        }); 
+}
+
+exports.filtrar = (request, response, next) => {
+    console.log(request.params.capybara_id);
+    //console.log(request.get('Cookie').split('=')[1]);
+    console.log(request.cookies);
+    const info = request.session.info ? request.session.info : '';
+    request.session.info = '';
+    Capybara.fetchOne(request.params.capybara_id)
+        .then(([rows, fieldData]) => {
+            console.log(rows);
+            response.render('lista', {
+                capybaras: rows,
+                username: request.session.username ? request.session.username : '',
+                ultimo_capybara: request.cookies.ultimo_capybara ? request.cookies.ultimo_capybara : '',
+                info: info //El primer info es la variable del template, el segundo la constante creada arriba
+            }); 
+        })
+        .catch(err => {
+            console.log(err);
+        }); 
--- APP1/models/tickets.js
+++ APP1/models/tickets.js
@@ -1,11 +1,13 @@
+const path = require('path');
 const db = require('../util/database');
 
 module.exports = class Ticket {
-
     //Constructor de la clase. Sirve para crear un nuevo objeto, y en él se definen las propiedades del modelo
-    constructor(ID_Ticket, description, preg1, preg2, preg3) {
+    constructor(ID_Ticket, titulo, categoria, descripcion, preg1, preg2, preg3) {
         this.id = ID_Ticket;
-        this.descripcion = description;
+        this.ti = titulo;
+        this.cat = categoria;
+        this.descripcion = descripcion;
         this.pregunta_uno = preg1;
         this.pregunta_dos = preg2;
         this.pregunta_tres = preg3;
@@ -13,8 +15,8 @@ module.exports = class Ticket {
 
     //Este método servirá para guardar de manera persistente el nuevo objeto. 
     save() {
-        return db.execute('INSERT INTO ticket (nombre, descripcion, imagen, duenio_id) VALUES (?, ?, ?, ?, ?)', 
-            [this.id, this.descripcion, this.pregunta_uno, this.pregunta_dos, this.pregunta_tres]);
+        return db.execute('INSERT INTO ticket (id, ti, cat, descripcion, pregunta_uno, pregunta_dos, pregunta_tres) VALUES (?, ?, ?, ?, ?)', 
+            [this.id, this.ti, this.cat, this.descripcion, this.pregunta_uno, this.pregunta_dos, this.pregunta_tres]);
     }
 
     //Este método servirá para devolver los objetos del almacenamiento persistente.
@@ -23,3 +25,4 @@ module.exports = class Ticket {
             'SELECT c.nombre, descripcion, imagen, duenio_id, c.created_at, u.nombre as duenio FROM capybaras c, usuarios u WHERE c.duenio_id = u.username');
     }
 }
+
