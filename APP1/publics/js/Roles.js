da9024899751cc7cd4ac745a4d24b3f3a78f6f21
Renato549 <A01275401@itesm.mx>
2022-03-29 09:50:12 -0600

    intento

---
+++ APP1/publics/js/Roles.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const user = require('../models/model_login');
+const bcrypt = require('bcryptjs');
+const { request } = require('http');
+
+
+let parameters = []
+
+
+const addJsonElement = json => {
+    parameters.push(json)
+    return parameters.length - 1
+}
+
+(function load(){
+    if(request.body.ID_rol == ){
+        const $btnAdd = document.getElementById("btnAdd")
+        const $form = document.getElementById("form")
+        const templateElement = () => {
+            return (`
+                <input required><span class="barra"></span>
+                <label for="">Pregunta </label>
+            `)
+        }
+        $btnAdd.addEventListener("click", (event) => {
+            const $div = document.createElement("div")
+            $div.classList.add("grupo")
+            $div.innerHTML = templateElement()
+            $form.insertBefore($div, $form.lastChild)
+        })
+    }
+
+})()
