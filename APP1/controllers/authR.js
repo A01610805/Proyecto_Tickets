73b4b31e658d5a104d62f92740cb5970bbce4957
Jose <A01610805@tec.mx>
2022-04-13 23:59:47 -0500

    nuevo diseño pag principal

--- APP1/controllers/authR.js
+++ APP1/controllers/authR.js
@@ -2,7 +2,7 @@ function authRole(role) {
     return (req, res, next) => {
         if (req.user.ID_rol !== role) {
             res.status(403)
-            console.log('esta madre funciona')
+            // console.log('esta madre funciona')
         }
     }
 }
