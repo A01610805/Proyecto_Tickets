34818a6f5da52b3e265b707aecf9d3127064e3bd
Renato549 <A01275401@itesm.mx>
2022-04-16 00:15:35 -0500

    conexion a asignar tickets

--- APP1/controllers/controller_asigPrio.js
+++ APP1/controllers/controller_asigPrio.js
@@ -1,38 +1,26 @@
-const Usuarios = require('../models/usuario');
+
 const Tickets = require('../models/tickets');
 
 
 
-exports.get_pant = (req, res, next) => {
-    console.log('Entrando a Buscar Usuario');
-    Usuarios.fetchEm()
-        .then(([rows, fielData]) => {
-            Usuarios.fetchAd()
-                .then(([rows2, fiellData]) => {
-                    Tickets.fetchticketsactivos()
-                        .then(([rows3, fiellData]) => {
-                            res.render('Asignar_Prioridad', {
-                                usuario: rows,
-                                usuario: rows2,
-                                tickets: rows3,
-        
-                            });
-                        })
-                })
-                .catch(error => { console.log(error) });
-        })
-        .catch(error => { console.log(error) });
-};
-
-exports.buscar = (req, res, next) => {
-    Usuarios.fetch(req.params.valor)
+exports.get_tic = (request, response, next) => {
+    console.log('Entrando a Asignar Prioridad');
+    Tickets.fetchticketSeleccionado(request.params.id)
         .then(([rows, fieldData]) => {
-            console.log(rows);
-            res.status(200).json(rows);
+            Tickets.fetchresTiseleccionado(request.params.id)
+                .then(([rows2, fieldData]) => {
+                    response.render('Asignar_Prioridad', {
+                        username: request.session.name ? request.session.name : '',
+                        respuestas: rows,
+                        tickets: rows2,
+                    });
+                })
         })
-        .catch(error => { console.log(error) });
-
 }
+exports.post_prioridad = (req, res, next) => {
+
+
+};
 
 // exports.buscarR = (req,res,next) =>{
 //     BusquedaR.fetchAll(req.params.valor)
@@ -47,4 +35,4 @@ exports.buscar = (req, res, next) => {
 exports.root = (req, res, next) => {
     console.log('Ruta por defecto de buscar usuario');
     res.redirect('/asignar_prioridad');
-};
+}
--- APP1/routes/route_asignarP.js
+++ APP1/routes/route_asignarP.js
@@ -3,11 +3,9 @@ const router = express.Router();
 
 const AP = require('../controllers/controller_asigPrio');
 
-router.get('/get_usti', AP.get_pant);
-// router.get('/get_preg/', AP.get_preguntas);
-// router.post('/camb_cat/', AP.post_mod);
-// // router.post('/n_preg/:id', AP.post_npreg);
-// // router.post('/de_preg/:id', AP.delete_preguntas);
+router.get('/get_tic/:id', AP.get_tic);
+router.get('/post_prio/:id', AP.post_prioridad);
+
 router.get('/', AP.root);
 
 module.exports = router; 
