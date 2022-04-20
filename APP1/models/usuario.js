f8e81d3abdfff58a47772152eeac3dc08c5b35c5
ATG-5222 <A01275222@tec.mx>
2022-04-20 16:28:51 -0500

    Paginación y filtros en buscar usuarios.

--- APP1/models/usuario.js
+++ APP1/models/usuario.js
@@ -7,10 +7,33 @@ module.exports = class Busqueda {
     static fetchAll() {
         return db.execute('SELECT ID_usuario, ID_rol, nombre, apellido_paterno, apellido_materno, correo, nombre_rol FROM usuario NATURAL JOIN rol');
     }
+
     static fetch(valor) {
-        return db.execute('SELECT ID_usuario, ID_rol, nombre, apellido_paterno, apellido_materno, correo, nombre_rol FROM usuario NATURAL JOIN rol WHERE (nombre LIKE ? OR apellido_paterno LIKE ? OR apellido_materno LIKE ? OR ID_usuario LIKE ? OR ID_rol LIKE ? OR correo LIKE ? OR nombre_rol LIKE ?)', ['%' + valor + '%', '%' + valor + '%', '%' + valor + '%', '%' + valor + '%', '%' + valor + '%', '%' + valor + '%', '%' + valor + '%']);
+        let arr = valor.split('&');
+        console.log(arr[0]);
+        console.log(arr[1]);
+        console.log(arr[2]);
+        return db.execute('SELECT ID_usuario, ID_rol, nombre, apellido_paterno, apellido_materno, correo, nombre_rol FROM usuario NATURAL JOIN rol WHERE ID_usuario LIKE ? AND nombre LIKE ? AND nombre_rol LIKE ?', ['%' + arr[0] + '%', '%' + arr[1] + '%', '%' + arr[2] + '%']);
     }
+
     static getidusuario(id) {
         return db.execute('SELECT ID_usuario FROM usuario WHERE correo=?', [id]);
     }
+
+    static getTotal_usuarios() {
+        return db.execute('SELECT count(*) as total FROM usuario')
+            .then(([rows, fieldData]) => {
+                console.log(rows[0].total);
+                return rows[0].total;
+            })
+            .catch(error => {
+                console.log(error);
+                return 0;
+            });
+    }
+
+    static fetchusuarios_pag(num) {
+        return db.execute('SELECT * FROM usuario NATURAL JOIN rol GROUP BY nombre ASC LIMIT ?, 5', [num]);
+    }
+
 }
--- APP1/routes/route_BuscarU.js
+++ APP1/routes/route_BuscarU.js
@@ -5,8 +5,10 @@ const bUser = require('../controllers/controller_BuscarU');
 const isAuth = require('../util/is-auth.js');
 const {authPage} = require('../util/rbac.js');
 
-router.get('/busqueda', isAuth, authPage(['1','2']), bUser.get_busqueda);
-router.get('/buscar/:valor', isAuth, authPage(['1','2']), bUser.buscar);
+router.get('/start/:start', isAuth, authPage(['1','2']), bUser.get_usuarios);
+router.get('/busqueda/:valor', isAuth, authPage(['1','2']), bUser.buscar);
+router.get('/busqueda', isAuth, authPage(['1','2']), bUser.get_usuarios);
+//router.post('/activos', tilogin.post_activos);
 router.get('/', isAuth, authPage([1,2]), bUser.root);
 
 module.exports = router; 
