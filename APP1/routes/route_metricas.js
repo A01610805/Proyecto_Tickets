55a3f81b59016a8f79c7947957bba6ae7580a06e
CamalichM <99758150+CamalichM@users.noreply.github.com>
2022-04-25 10:49:17 -0500

    Corrección de autentificación

--- APP1/routes/route_metricas.js
+++ APP1/routes/route_metricas.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const metricas = require('../controllers/controller_metricas');
+const isAuth = require('../util/is-auth.js');
 
-router.get('/', metricas.getmetricas);
+router.get('/', isAuth, metricas.getmetricas);
 
 module.exports = router;
