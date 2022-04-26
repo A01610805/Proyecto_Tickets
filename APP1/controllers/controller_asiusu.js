12875297cfae05fe3c27c268dccb9a3202e02d9b
CamalichM <99758150+CamalichM@users.noreply.github.com>
2022-04-26 13:35:23 -0500

    Ruta de asignar ticket a usuario ya sirve

--- APP1/controllers/controller_asiusu.js
+++ APP1/controllers/controller_asiusu.js
@@ -1,4 +1,20 @@
+const Usuario = require('../models/usuario');
 
-exports.get_usuario = (request, response, next) => {}
+exports.get_usuario = (request, response, next) => {
+    console.log(request.params.id);
+    console.log('Entrando al render de asignar ticket a usuario');
+    Usuario.fetchusuario_id(request.params.id)
+        .then(([rows, fieldData]) => {
+            console.log(rows);
+            response.render('asiusu', {
+                id: request.params.id ? request.params.id : 1,
+                username: request.cookies.nombre_usuario ? request.cookies.nombre_usuario : '',
+                rol: request.cookies.rolusuario ? request.cookies.rolusuario : 3,
+                user: rows[0],
+            });
+        })
+        .catch(error => { console.log(error) });  
+}
 exports.post_asiusu = (request, response, next) => {}
-exports.root = (request, response, next) => {}
+exports.root = (request, response, next) => {
+res.redirect('/asignar');}
--- APP1/routes/route_asiusu.js
+++ APP1/routes/route_asiusu.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const asiusu = require('../controllers/controller_asiusu');
 
-router.get('/get_usuario/:id', asiusu.get_usuario);
+router.get('/asignar/:id', asiusu.get_usuario);
 router.post('/post_asiusu/:id', asiusu.post_asiusu);
 router.get('/', asiusu.root);
 
