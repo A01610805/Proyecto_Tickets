08101e3b6a45b1df81c127863d3afedc7a34b3c1
Jose <A01610805@tec.mx>
2022-04-26 20:25:31 -0500

    intento de db

--- zebrands_tickets.sql
+++ zebrands_tickets.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 27-04-2022 a las 02:30:52
+-- Tiempo de generación: 27-04-2022 a las 03:16:47
 -- Versión del servidor: 10.4.14-MariaDB
 -- Versión de PHP: 7.4.11
 
