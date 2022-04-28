ce9dac837f2824d3d70cf558371ab011783f2483
CamalichM <99758150+CamalichM@users.noreply.github.com>
2022-04-28 09:18:54 -0500

    Dropdown funcional en asignar usuario a ticket

--- APP1/routes/route_asiusu.js
+++ APP1/routes/route_asiusu.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const asiusu = require('../controllers/controller_asiusu');
 
 router.get('/asignar/:id', asiusu.get_usuario);
-router.post('/post_asiusu/:id', asiusu.post_asiusu);
+router.post('/asignar/:id', asiusu.post_asiusu);
 router.get('/', asiusu.root);
 
 module.exports = router;  
