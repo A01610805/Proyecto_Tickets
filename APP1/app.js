d367687fa310f669a4b41b9eb581238b36846f7a
FlavioRr <A01367631@itesm.mx>
2022-04-28 10:21:01 -0500

    favicon funciona

--- APP1/app.js
+++ APP1/app.js
@@ -28,7 +28,7 @@ app.use(session({
     saveUninitialized: false, //Asegura que no se guarde una sesión para una petición que no lo necesita
 }));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'publics')));
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
