7cf644d1d51acb000ac0499344cac8b31f2845b7
Jose <A01610805@tec.mx>
2022-05-04 17:23:58 -0500

    se comentan logs de models

--- APP1/models/usuario.js
+++ APP1/models/usuario.js
@@ -18,9 +18,9 @@ module.exports = class Busqueda {
 
     static fetch(valor) {
         let arr = valor.split('&');
-        console.log(arr[0]);
-        console.log(arr[1]);
-        console.log(arr[2]);
+        // console.log(arr[0]);
+        // console.log(arr[1]);
+        // console.log(arr[2]);
         return db.execute('SELECT ID_usuario, ID_rol, nombre, apellido_paterno, apellido_materno, correo, nombre_rol FROM usuario NATURAL JOIN rol WHERE ID_usuario LIKE ? AND nombre LIKE ? AND nombre_rol LIKE ?', ['%' + arr[0] + '%', '%' + arr[1] + '%', '%' + arr[2] + '%']);
     }
 
@@ -31,7 +31,7 @@ module.exports = class Busqueda {
     static getTotal_usuarios() {
         return db.execute('SELECT count(*) as total FROM usuario')
             .then(([rows, fieldData]) => {
-                console.log(rows[0].total);
+                // console.log(rows[0].total);
                 return rows[0].total;
             })
             .catch(error => {
