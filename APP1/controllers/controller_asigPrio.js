3df341c8a1dc1040624c70edcc70a353163babea
Jose <A01610805@tec.mx>
2022-05-05 11:14:40 -0500

    error asigna prioridad

--- APP1/controllers/controller_asigPrio.js
+++ APP1/controllers/controller_asigPrio.js
@@ -18,28 +18,14 @@ exports.get_tic = (request, response, next) => {
 };
 
 exports.post_prio = (request, response, next) => {
-    // console.log('hola?');
     const prioridad = new Prioridad(request.body.prioridad, request.params.id);
-    // console.log(prioridad);
-    // console.log('Iniciando update');
     prioridad.update()
-    //prioridad.updatenuevo()
-    // console.log('Terminando update');
+    prioridad.updatenuevo()
     response.redirect('/home')
 
 };
 
-// exports.buscarR = (req,res,next) =>{
-//     BusquedaR.fetchAll(req.params.valor)
-//         .then(([rows, fieldData]) => {
-//             console.log(rows);
-//             res.status(200).json(rows);
-//         })
-//         .catch(error => {console.log(error)});  
-
-// }
 
 exports.root = (req, res, next) => {
-    // console.log('Ruta por defecto de buscar usuario');
     res.redirect('/asignar_prioridad');
 }
