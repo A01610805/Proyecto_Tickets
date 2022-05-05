7b6435a5dc5c26e2e8551be65ca32c6402b07e72
Renato549 <A01275401@itesm.mx>
2022-05-05 01:12:15 -0500

    algo

--- APP1/util/database.js
+++ APP1/util/database.js
@@ -5,6 +5,7 @@ const DBlocal = {
     user: 'root',
     database: 'zebrands_tickets',
     password: '',
+    port: 3307
 };
 
 const DBinstancia = {
@@ -15,6 +16,6 @@ const DBinstancia = {
     port: 3306
 }
 
-const pool = mysql.createPool(DBinstancia);
+const pool = mysql.createPool(DBlocal);
 
 module.exports = pool.promise();
