e9ee8f8945015d04403b8f939ba8a1637329a34a
Jose <A01610805@tec.mx>
2022-05-06 00:38:33 -0500

    corrección gráfica 1

--- APP1/models/metricas.js
+++ APP1/models/metricas.js
@@ -11,13 +11,13 @@ module.exports = class Metricas {
     // }
     static tiemposCat() {
         return db.execute(
-            'SELECT (AVG((UNIX_TIMESTAMP(fecha_fin) - UNIX_TIMESTAMP(fecha_emision)) DIV 86400)) as info_metrica FROM categoria C, ticket T, genera_ticket GT, resuelve_ticket RT WHERE T.ID_categoria = C.ID_categoria AND T.ID_ticket = GT.ID_ticket AND T.ID_ticket = RT.ID_ticket GROUP BY T.ID_categoria ORDER BY nombre_categoria ASC'
+            'SELECT (AVG((UNIX_TIMESTAMP(fecha_fin) - UNIX_TIMESTAMP(fecha_emision)) DIV 86400)) as info_metrica FROM categoria C, ticket T, genera_ticket GT, resuelve_ticket RT WHERE T.ID_categoria = C.ID_categoria AND T.ID_ticket = GT.ID_ticket AND T.ID_ticket = RT.ID_ticket AND WHERE disponible = 1 GROUP BY T.ID_categoria ORDER BY nombre_categoria ASC'
         );
     }
 
     static nombresCat() {
         return db.execute(
-            'SELECT nombre_categoria FROM categoria C ORDER BY nombre_categoria ASC'
+            'SELECT nombre_categoria FROM categoria C WHERE disponible = 1 ORDER BY nombre_categoria ASC'
         );
     }
 
